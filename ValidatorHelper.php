77febac825dcce93a1b105bedac8b5a2dd17a016
chieff <itsabodyshotsc@mail.ru>
2024-03-03 13:07:37 +0300

    phone and ip validators

--- ValidatorHelper.php
+++ ValidatorHelper.php
@@ -78,4 +78,53 @@ class ValidatorHelper {
         return $value;
     }
 
+    /**
+     * @param string $phone
+     * @param int $length
+     * @return bool
+     */
+    public static function validatePhone(string $phone, int $length = 11)
+    {
+        if (!$phone)
+            return false;
+
+        preg_match_all('/[0-9]+/', $phone, $matches);
+
+        if (!$matches || !isset($matches[0]) || !$matches[0])
+            return false;
+
+        $phone = implode('', $matches);
+
+        if (mb_strlen($phone) != $length)
+            return false;
+
+        return true;
+    }
+
+    /**
+     * @param string $ip
+     * @param bool $portCheck
+     * @param bool $portEmpty
+     * @return bool
+     */
+    public static function validateIp(string $ip, bool $portCheck = true, bool $portEmpty = true)
+    {
+        if (!$ip)
+            return false;
+
+        $regexp = '/^(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[0-9]{2}|[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[0-9]{2}|[0-9])){3}';
+
+        if ($portCheck && !$portEmpty)
+            $regexp .= ':[0-9]{1,6}$/';
+        else if ($portCheck && $portEmpty)
+            $regexp .= '(|:[0-9]{1,6})$/';
+        else
+            $regexp .= '$/';
+
+        if (preg_match('', $ip) !== 1)
+            return false;
+
+        return true;
+    }
+
 }
